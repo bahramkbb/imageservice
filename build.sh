bf2610e677b500bb7883828f7087dd7db5e72d09
Bahram Karimi <bahramkb@gmail.com>
2018-05-24 17:06:15 +0430

    Initial commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+habitus
+docker-compose up -d
--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-habitus
-docker push bahramkb/imageservice
 
-#ssh deploy@SWARMIP 
-#docker-machine ssh cluster01 << EOF
-#docker pull bahramkb/imageservice:latest
-#docker rmi bahramkb/imageservice:current || true
-#docker tag bahramkb/imageservice:latest bahramkb/imageservice:current
-#EOF
-
-docker-machine ssh cluster01 << EOF
-mkdir /swarm || true
-cd /swarm
-git clone https://www.github.com/bahramkb/imageservice || true
+ssh -A deploy@172.25.30.46 '/bin/bash -s' << EOF
+docker pull bahramkb/imageservice:latest
+docker rmi bahramkb/imageservice:current || true
+docker tag bahramkb/imageservice:latest bahramkb/imageservice:current
+mkdir ~/swarm || true
+cd ~/swarm
+echo 'yes\n' | git clone git@github.com:bahramkarimi/imageservice.git ||
+cd imageservice
 git pull || true
 docker stack deploy -c docker-compose.yml imageservice
 EOF
